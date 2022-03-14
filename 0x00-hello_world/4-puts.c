842c65d36590455abd612f15a33e7cfac49089d2
daposarumi <sarumiadedapo@gmail.com>
2022-03-14 14:30:18 -0700

    4-put.c added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+
+/**
+ * main - Entry point of our program
+ *
+ * This is the main functions, where everything starts
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
