c8271faf749b0cbdf086406fcd5d8d2c4b888573
daposarumi <sarumiadedapo@gmail.com>
2022-03-15 08:40:22 -0700

    5-printf.c added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,12 @@
 
 /**
  * main - Entry point
+ *
  * Description: prints "with proper grammar, but the outcome is a piece of art"
  * return: 0
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0); }
