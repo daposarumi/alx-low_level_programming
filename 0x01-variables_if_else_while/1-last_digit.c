4a75560fd0a8f13b5a5f4b32cb826cecbbbe1a61
daposarumi <sarumiadedapo@gmail.com>
2022-09-09 07:39:05 -0700

    Remove whitespace

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 5)
-		printf("Last string of n is %d and is greater th		an 5\n", n);
+		printf("Last string of n is %d and is greater than 5\n", n);
 	else if (n == 0)
-		printf("Last string of n is %d and is zero\n", n		);
+		printf("Last string of n is %d and is zero\n", n);
 	else if (n < 6 && n != 0)
-		printf("Last string of n is %d and is less than 		6 and not 0\n", n);
+		printf("Last string of n is %d and is less than 6 and not 0\n", n);
 
 	return (0);
 }	
