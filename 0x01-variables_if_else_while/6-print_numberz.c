ca5cdbec069956b2b60f18998f629a5bfb9e81a4
daposarumi <sarumiadedapo@gmail.com>
2022-09-09 09:25:54 -0700

    print numbers easily

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ * Description: print numbers one to
+ * nine in a hard way
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
