26b68e54a48c04f137ac6e071a3d521da378b5fc
daposarumi <sarumiadedapo@gmail.com>
2022-09-09 16:48:10 -0700

    Add script that prints three combos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - function prints combo of 3 digits
+ * Return: 0
+ */
+int main(void)
+{
+	int tens, ones;
+
+	for (tens = 0; tens <= 99; tens++)
+	{
+		for (ones = 0; ones <= 99; ones++)
+		{
+			if (tens < ones)
+			{
+				putchar((tens / 10) + 48);
+				putchar((tens % 10) + 48);
+				putchar(' ');
+				putchar((ones / 10) + 48);
+				putchar((ones % 10) + 48);
+				if (tens != 98 || ones != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
