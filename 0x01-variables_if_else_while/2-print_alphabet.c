cecfd9dd171e6d1e1066a64d70e084c7056f4c97
daposarumi <sarumiadedapo@gmail.com>
2022-09-09 08:36:24 -0700

    Add script that prints alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,4 +2,19 @@
 #include <ctype.h>
 
 /**
- *
+ * main - Entry point
+ * Description: prints all alphabetss in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	int lower_x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		lower_x = tolower(x);
+		putchar(lower_x);
+	}
+	return (0);
+}
