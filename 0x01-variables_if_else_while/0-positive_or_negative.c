b9cf19f2f30f7c37015e6e1af6c354d110ac0faf
daposarumi <sarumiadedapo@gmail.com>
2022-09-09 06:45:34 -0700

    Add new scripts

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
-#include <stdlibghh>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 
 /**
  *  main - beginning of program
- *  @void - no arguments
+ *  @void - no parameters
  *
  *  Description - function checks if number
  *  is positive or negative
@@ -23,9 +23,8 @@ int main(void)
 		printf("%d is negative\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else 
+	else
 		printf("%d is positive\n", n);
 
 	return (0);
-
 }
