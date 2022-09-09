7ffc09b01549a055a8a68d36b2704244325c721f
daposarumi <sarumiadedapo@gmail.com>
2022-09-09 07:48:15 -0700

    Remove whitespace and do Betty's will

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,10 +22,10 @@ int main(void)
 	last_num = n % 10;
 
 	if (n > 5)
-		printf("Last string of %i is %d and is greater than 5\n", n, last_num);
+		printf("Last digit of %i is %d and is greater than 5\n", n, last_num);
 	else if (n == 0)
-		printf("Last string of %i is %d and is zero\n", n, last_num);
+		printf("Last digit of %i is %d and is zero\n", n, last_num);
 	else if (n < 6 && n != 0)
-		printf("Last string of %i is %d and is less than 6 and not 0\n", n, last_num);
+		printf("Last digit of %i is %d and is less than 6 and not 0\n", n, last_num);
 	return (0);
 }	
