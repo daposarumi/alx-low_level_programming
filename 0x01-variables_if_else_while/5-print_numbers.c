77a91fbee2c2784ac990c06ea0aa47398d511d82
daposarumi <sarumiadedapo@gmail.com>
2022-09-09 09:15:31 -0700

    Add script that prints 1 t0 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ * Description: program print numbers
+ * from one to nine
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+	
+	return (0);
+}
