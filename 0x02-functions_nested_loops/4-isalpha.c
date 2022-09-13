a002c56095946616a96f416ef113bbe31ed0c826
daposarumi <sarumiadedapo@gmail.com>
2022-09-13 09:22:12 -0700

    fix space

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,12 +7,12 @@
  * alphabetic character
  * @c: character to check
  *
- * Return 0 or 1
+ * Return 1 if c is a letter, lowercase or uppercase else 0.
  */
 int _isalpha(int c)
 {
 	if isalpha(c)
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
