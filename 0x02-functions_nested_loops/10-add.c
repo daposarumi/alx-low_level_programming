598ddca6c83ffbf5d64aafe1aa38f8ad533a8663
daposarumi <sarumiadedapo@gmail.com>
2022-09-13 17:21:18 -0700

    Add script that adds integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * add - A function that adds two integers.
+ * @a: integer number
+ * @b: integer number
+ *
+ * Return: Result of the addition
+ */
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+
+	return (sum);
+}
