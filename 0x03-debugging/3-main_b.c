885b6a5f7b01616ad92ad2c508f0283fccd1ad43
daposarumi <sarumiadedapo@gmail.com>
2022-09-15 19:23:30 +0000

    Add file

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -2,13 +2,16 @@
 #include "main.h"
 
 /**
- * main - takes a date and prints how many days are left in the year
- * taking leap years into account
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
  * Return: 0
  */
+
 int main(void)
 {
-	int month, day, year;
+	int month;
+	int day;
+	int year;
 
 	month = 4;
 	day = 01;
