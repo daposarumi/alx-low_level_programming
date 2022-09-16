02756444f33895d6cf4948b9e6b622dcdeb32bdb
daposarumi <sarumiadedapo@gmail.com>
2022-09-16 13:00:16 +0000

    Add diagonal file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws diagonal
+ * @n: number of times
+ *
+ * Return: no return
+ */
+void print_diagonal(int n)
+{
+	int a = 0;
+	int b = 0;
+
+	if (n > 0)
+	{
+		while (a < n)
+		{
+			while (b < a)
+			{
+				_putchar(' ');
+				b++;
+			}
+			a++;
+			b = 0;
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
