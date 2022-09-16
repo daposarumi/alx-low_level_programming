cd1cd5bd430f91e30ab2dd3a1640582affd21d7a
daposarumi <sarumiadedapo@gmail.com>
2022-09-16 10:55:16 +0000

    Add function that prints multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a - first int.
+ * @b - second integer
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int cross;
+	cross = a * b;
+	return (cross);
+}
