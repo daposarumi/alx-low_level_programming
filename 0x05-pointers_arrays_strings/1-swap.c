13ccb1b27068ed8b137c7fc9d2e28e764a0c626b
daposarumi <sarumiadedapo@gmail.com>
2022-09-19 20:55:20 +0000

    Add function that swaps numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - swaps integers
+ * @*a: first pointer
+ * @*b: second pointer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int new = *a;
+	*a = *b;
+	*b = new;
+}
