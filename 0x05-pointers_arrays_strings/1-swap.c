a1fb071bab36d8e3a8e5f119e1a0d6abf96583dc
daposarumi <sarumiadedapo@gmail.com>
2022-09-19 21:09:01 +0000

    add

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,13 +3,8 @@
 
 /**
  * swap_int - swaps integers
-<<<<<<< HEAD
- * @*a: integer
- * @*b: integer
-=======
- * @a: first pointer
- * @b: second pointer
->>>>>>> 0a0915b2cb1f690966b09f184dca736c929dc2a0
+ * @a: integer
+ * @b: integer
  *
  * Return: void
  */
