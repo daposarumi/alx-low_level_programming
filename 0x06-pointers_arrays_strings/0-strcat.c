5957568a5df6fe770506a62f70f11e897a3c0a42
daposarumi <sarumiadedapo@gmail.com>
2022-09-21 14:24:36 +0000

    Add new file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates
+ * strings
+ * @dest: first string
+ * @src: second
+ *
+ * Return: pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
