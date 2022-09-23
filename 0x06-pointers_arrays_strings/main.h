b7d768ef60c67299b8034c0073d4b2c527f51aef
daposarumi <sarumiadedapo@gmail.com>
2022-09-23 11:41:30 +0000

    Add func that prints buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,3 +11,6 @@ void print_number(int n);
 int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+void printASCII(char *b, int start, int end);
+void printHexes(char *b, int start, int end);
+int isPrintableASCII(int n);
