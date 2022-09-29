2d6ecdeb99d86036c00a12cea56fd86bb5044475
daposarumi <sarumiadedapo@gmail.com>
2022-09-29 15:54:41 +0000

    Add function that prints factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - function prints factorial of number
+ * @n: integer
+ *
+ * Return: 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return -1;
+	else if (n == 0)
+		return 1;
+	else
+		return (n * factorial(n-1));
+	_putchar('\n');
+}
