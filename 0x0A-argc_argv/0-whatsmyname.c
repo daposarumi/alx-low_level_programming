7c730170c3aa6145bc720656a981ceb297c06782
daposarumi <sarumiadedapo@gmail.com>
2022-09-30 08:46:17 -0700

    Add latest

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function prints the name of the argument
+ * @argc: count
+ * @argv: pointer to array
+ *
+ * Return: 0
+ */
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
