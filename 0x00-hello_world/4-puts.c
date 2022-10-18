56c85af8e22c306f5ffc44b96fd913e6ff26c753
daposarumi <sarumiadedapo@gmail.com>
2022-10-18 11:58:12 +0000

    Edit puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,17 @@
 
 
 /**
- * main - Entry point
+ * main - function prints string with puts
  * @void - Nothing
  *
  * Description: prints "Programming is like building a multilingual puzzle"
+ * with puts
+ *
  * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0); }
+	return (0);
+}
