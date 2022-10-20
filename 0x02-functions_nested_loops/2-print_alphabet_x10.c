8ba0438aa325f1929176ea3da847cafab2c148bb
daposarumi <sarumiadedapo@gmail.com>
2022-10-20 19:02:12 +0000

    Edit function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,19 @@
  * Description: Function prints 10 times
  * the alphabet, in lowercase,
  * followed by a new line
+ *
  * Return: 0
  *
  */
 void print_alphabet_x10(void)
 {
-	int x, a, lower_x;
+	int x, a;
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (x = 'A'; x <= 'Z'; x++)
+		for (x = 'a'; x <= 'z'; x++)
 		{
-			lower_x = tolower(x);
-			_putchar(lower_x);
+			_putchar(x);
 		}
 	_putchar('\n');
 	}
