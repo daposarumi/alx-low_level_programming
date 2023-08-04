6326bdf8d1037dc414ca5820185a7b055de66045
daposarumi <sarumiadedapo@gmail.com>
2023-08-04 23:10:36 +0000

    new

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - function returns length of string
+ * @s: pointer to string
+ * Description: function returns length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+
